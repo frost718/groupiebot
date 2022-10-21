602d8d625f67bc0428a7914cb4d9639874c77402
Erik Gostischa-Franta <erik.gostischa-franta@rise-world.com>
2022-10-21 23:41:06 +0200

    adds discord groupie bot

---
+++ groupiediscordbot.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to calculate layered limit orders for two extreme points on a chart
+
+Usage:
+# python3 groupiediscordbot.py
+Press Ctrl-C on the command line or send a signal to the process to stop the bot.
+"""
+
+import re
+import yaml
+import range_calc
+
+import discord
+from discord.ext import commands
+
+# Config Properties
+with open('config.yml', 'rb') as f:
+    config = yaml.safe_load(f)
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.command()
+async def layered(ctx, *, message: str):
+    args = message.split()
+    if len(args) != 6:
+        await ctx.send("Expecting a total of 6 parameters, please specify them, see /help")
+        return
+    range_strategy = args[0]
+    size_strategy = args[1]
+    size_param = args[2]
+    size_amount = args[3]
+    range1 = args[4]
+    range2 = args[5]
+    await ctx.send(re.sub(r'<.*?>', '', range_calc.layered(range_strategy, size_strategy, size_param, size_amount, range1, range2)))
+
+
+@bot.command()
+async def layered_help(ctx):
+    """Send a message when the command /help is issued."""
+    with open("help.html", "r") as text_file:
+        help_text = text_file.read()
+    await ctx.send(re.sub(r'<.*?>', '', help_text))
+
+
+bot.run(config['discord_bot_token'])
